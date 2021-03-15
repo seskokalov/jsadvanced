ca0cfb351251a02b0e53dfdf9db9922190378e2a
Nikola Sheskokalov <78028357+seskokalov@users.noreply.github.com>
2021-03-15 20:59:55 +0100

    Add files via upload

---
+++ FinalClass/script.js
@@ -0,0 +1,36 @@
+let countryInput = document.getElementById("countryInput");
+let searchBtn = document.getElementById("searchBtn");
+let result = document.getElementById("result");
+let country
+
+const baseUrl = "https://restcountries.eu/rest/v2/name/";
+
+function getCountry(code) {
+    return fetch(baseUrl + code)
+        .then(response => response.json())
+        .catch(error => console.error(error))
+    
+}
+
+
+
+// # Build your own app 
+// ## CountrySearch
+// CountrySearch is an application that is meant to search for countries and get data for them in real time. The application is very simple. It only has one functionality: Search and show the countries in a table
+// ### Requirements
+// * There should be one search input to input name or partial name of a country
+// * There should be a button for search to initiate
+// * When the button is clicked, a table shows with the possible answers
+// * The table shows:
+//   * Flag
+//   * Name
+//   * Population
+//   * Capital
+//   * Area
+// * The API for countries is: https://restcountries.eu/
+// * Read the API documentation in order to figure out how to call for the countries
+// ### Extra requirements
+//   * List language names and currency names in the table as well ( only names and divided by , EX: Spanish, English )
+//   * Add loading image while it is getting the data
+//   * Add a filter by name, area and population in descending order
+//   * Add a filter by name, area and population in ascending order
